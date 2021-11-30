3c168ab5dcb0671fd6cf131b38db9359a051f8fe
rmzntkn <rmtekin@outlook.com>
2021-11-30 16:52:53 -0500

    asd

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -5,12 +5,14 @@ import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import Greet from "./components/Greet";
 import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Message />
+				<Counter />
+				{/* <Message /> */}
 
 				{/* <Greet name="Ramazan" heroname="Tekin">
 					<p>This is children props</p>
