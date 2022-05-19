4acab463ae24d05a9fe6178ec428645e55f7f18a
rmzntkn <rmtekin@outlook.com>
2022-05-18 23:27:15 -0400

    Update script.js

--- javascript/leetcode/script.js
+++ javascript/leetcode/script.js
@@ -359,17 +359,90 @@
 ////////////////////////
 ////////////////////////
 //171. Excel Sheet Column Number
-var titleToNumber = function (s) {
-  let result = 0;
+// var titleToNumber = function (s) {
+//   let result = 0;
+//   for (let i = 0; i < s.length; i++) {
+//     console.log('rest', s.charCodeAt(i) - 64);
+//     //console.error('resutl', (result = result * 26 + s.charCodeAt(i) - 64));
+//   }
+//   return result;
+// };
+// console.log('ABC', titleToNumber('ABC'));
+
+// let rt = 'ABC';
+
+// let tr = rt.charCodeAt(2) - 64;
+// console.log(tr);
+
+// var lengthOfLongestSubstring = function (s) {
+//   for(let i=0; i<s.length; i++){
+//      for(let j =1; j<s.length; j++){
+//        if(s[i}===)
+//      }
+//     ;
+//   }
+// };
+
+// var lengthOfLongestSubstring = function (s) {
+//   for (let i = 0; i < s.length; i++) {
+//     for (let j = 1; j < s.lenght; j++) {
+//       if (s[i] === s[j]) {
+//         return s.splice(j, 1);
+//       }
+//     }
+//   }
+//   return s;
+// };
+// console.log(lengthOfLongestSubstring('abcabcbb'));
+
+////////////////////////
+////////////////////////
+////////////////////////
+// Consoel only 1 repeat items only lower case letters
+// let s = 'abcabcbbv';
+// function lengtRemoveDup(s) {
+//   let arr = new Array(26);
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i] = 0;
+//   }
+
+//   for (let i = 0; i < s.length; i++) {
+//     arr[s.charAt(i).charCodeAt(0) - 97]++;
+//   }
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == 1) {
+//       console.log(String.fromCharCode(97 + i));
+//     }
+//   }
+// }
+
+// lengtRemoveDup(s);
+
+////////////////////////
+////////////////////////
+////////////////////////
+// return items only 1 times in new array
+// let s = 'abcabcbbv';
+// function removeDublicate(s) {
+//   let arr = [];
+//   for (let i = 0; i < s.length; i++) {
+//     if (!arr.includes(s[i])) {
+//       arr.push(s[i]);
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log(removeDublicate(s));
+
+////////////////////////
+////////////////////////
+////////////////////////
+// return items only 1 times in same array
+let s = 'abcabcbbv';
+function removeDublicate(s) {
   for (let i = 0; i < s.length; i++) {
-    console.log('rest', s.charCodeAt(i) - 64);
-    //console.error('resutl', (result = result * 26 + s.charCodeAt(i) - 64));
+    s = s.splice(i, 1);
   }
-  return result;
-};
-console.log('ABC', titleToNumber('ABC'));
-
-let rt = 'ABC';
-
-let tr = rt.charCodeAt(2) - 64;
-console.log(tr);
+}
